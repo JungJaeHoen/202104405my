418466702c7a54614bc97e7a9582343695736b94
JungJaeHoen <ctrlzero2@naver.com>
2021-09-08 12:28:16 +0900

    delete java hello

--- JSP_git/src/com/java/main/Main.java
+++ JSP_git/src/com/java/main/Main.java
@@ -3,8 +3,5 @@ package com.java.main;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("main");
-		System.out.println("java"); 
-		
-		System.out.println("hello");
 	}
 }
